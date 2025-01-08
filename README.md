85522dd2735f684522401db9ada81d7b45b60665
YoahnDa <143202366+YoahnDa@users.noreply.github.com>
2025-01-08 19:40:37 +0300

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Baovola
-Projet Gestion d'entreprise
---
+++ baovola/src/main/java/com/baovola/baovola/BaovolaApplication.java
@@ -0,0 +1,13 @@
+package com.baovola.baovola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaovolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaovolaApplication.class, args);
+	}
+
+}

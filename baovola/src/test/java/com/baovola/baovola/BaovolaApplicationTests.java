4412dd1377445766c7ff98e582f5cddcd8daad97
ampinga <ratsimbapia@gmail.com>
2025-01-08 20:47:17 +0300

    front
    Co-Authored-By: YoahnDa <143202366+YoahnDa@users.noreply.github.com>

---
+++ baovola/src/test/java/com/baovola/baovola/BaovolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baovola.baovola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaovolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

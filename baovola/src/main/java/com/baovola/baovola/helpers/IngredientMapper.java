b34461420b92adaa63d06fcac4c3b51af30af816
ampinga <ratsimbapia@gmail.com>
2025-01-17 14:25:56 +0300

    pings_branche

--- baovola/src/main/java/com/baovola/baovola/helpers/IngredientMapper.java
+++ baovola/src/main/java/com/baovola/baovola/helpers/IngredientMapper.java
@@ -1,5 +1,6 @@
 package com.baovola.baovola.helpers;
 
+<<<<<<< Updated upstream
 import com.baovola.baovola.models.Unite;
 import com.baovola.baovola.models.MatierePremiere;
 import com.example.demo.dto.ProductDTO;
@@ -10,6 +11,15 @@ import javax.xml.stream.events.EntityDeclaration;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
+=======
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.baovola.baovola.dto.IngredientDto;
+import com.baovola.baovola.models.MatierePremiere;
+
+@Mapper(componentModel = "spring" , uses = UniteMapper.class)
+>>>>>>> Stashed changes
 public interface IngredientMapper {
     @Mapping(source = "unite", target = "unite")
     MatierePremiere toEntity(IngredientDto dto);

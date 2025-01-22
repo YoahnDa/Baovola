7b77e38765f754f871cd0c6e970aa7b482743338
YoahnDa <143202366+YoahnDa@users.noreply.github.com>
2025-01-22 08:27:59 +0300

    Premier modif categorie

---
+++ baovola/src/main/java/com/baovola/baovola/controllers/CategorieRestController.java
@@ -0,0 +1,9 @@
+package com.baovola.baovola.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categorie")
+public class CategorieRestController {
+
+}
